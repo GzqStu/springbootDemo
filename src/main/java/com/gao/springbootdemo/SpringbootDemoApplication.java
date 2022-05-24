3bad7dcaab5489b4a937a8098a1b670e62bfd31f
DESKTOP-0LUK01P\Mr.Gao <2538120593@qq.com>
2022-05-24 09:42:30 +0800

    增加心跳接口

--- src/main/java/com/gao/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/gao/springbootdemo/SpringbootDemoApplication.java
@@ -2,12 +2,20 @@ package com.gao.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
 
+    @RequestMapping(value = "/health", method = RequestMethod.GET)
+    public String health() {
+        return "hello springboot";
+    }
 }
