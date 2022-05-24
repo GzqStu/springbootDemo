896ce30a09ae2295946001b0e60a763c6cf33847
DESKTOP-0LUK01P\Mr.Gao <2538120593@qq.com>
2022-05-24 11:57:21 +0800

    配置mysql数据库配置

--- src/main/java/com/gao/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/gao/springbootdemo/SpringbootDemoApplication.java
@@ -1,14 +1,12 @@
 package com.gao.springbootdemo;
 
-import lombok.ToString;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @RestController
 public class SpringbootDemoApplication {
 
